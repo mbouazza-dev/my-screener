abc16cdc5b3eec93b12b7bfbe3b5a5ad079c9b76
mbouazza-dev <mbouazzapro@gmail.com>
2025-08-26 12:16:55 +0200

    Add every kpi (need to update right text area later)

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import requests
 import smtplib
+import streamlit as st
 from email.mime.text import MIMEText
 
+st.set_page_config(layout="wide")
 BASE_URL = f"{'https://api.coingecko.com/api/v3/'}"
+API_KEY = "CG-Jk87wLHxvU3NbJoNShiaxksU"
 
+headers = {
+    "accept": "application/json",
+    "x-cg-demo-api-key": API_KEY
+}
 
 # 1. Total marketcap alcoins (except BTC & ETH)
 def get_total3_marketcap():
     url = f"{BASE_URL}/global"
-    r = requests.get(url)
+    r = requests.get(url, headers=headers)
     data = r.json()
     total_marketcap = data["data"]["total_market_cap"]["usd"]
     btc_marketcap = data["data"]["market_cap_percentage"]["btc"] * total_marketcap / 100
@@ -19,7 +26,7 @@ def get_total3_marketcap():
 # 2. Altcoins volume
 def get_total3_volume():
     url = f"{BASE_URL}/global"
-    r = requests.get(url)
+    r = requests.get(url, headers=headers)
     data = r.json()
     total_volume = data["data"]["total_volume"]["usd"]
     return {"total_volume_usd": total_volume}
@@ -27,7 +34,7 @@ def get_total3_volume():
 # 3. Trends
 def get_trending_coins():
     url = f"{BASE_URL}/search/trending"
-    r = requests.get(url)
+    r = requests.get(url, headers=headers)
     data = r.json()
     trending = [coin["item"]["name"] for coin in data["coins"]]
     return trending
@@ -35,25 +42,24 @@ def get_trending_coins():
 # 4. Fear & Greed Index
 def get_fear_greed():
     url = "https://api.alternative.me/fng/"
-    r = requests.get(url)
+    r = requests.get(url, headers=headers)
     data = r.json()
     return {"value": data["data"][0]["value"], "classification": data["data"][0]["value_classification"]}
 
 # 5. Rotation sectorielle
 def get_top_sectors():
     url = f"{BASE_URL}/coins/categories"
-    r = requests.get(url)
+    r = requests.get(url, headers=headers)
     data = r.json()
-    top_categories = sorted(data, key=lambda x: x["market_cap"], reverse=True)[:5]
-    return [{"name": cat["name"], "market_cap": cat["market_cap"]} for cat in top_categories]
+    return [{"name": cat["name"], "market_cap": cat["market_cap"]} for cat in data[:5]]
 
-# BTC.D
+# 6. BTC.D
 def get_btc_dominance():
     url = f"{BASE_URL}/global"
     data = requests.get(url).json()
     return data["data"]["market_cap_percentage"]["btc"]
 
-# ETH/BTC
+# 7. ETH/BTC
 def get_eth_btc_ratio():
     url = f"{BASE_URL}/simple/price?ids=bitcoin,ethereum&vs_currencies=usd"
     data = requests.get(url).json()
@@ -71,7 +77,6 @@ def send_alert(message):
         server.login("mehdbest.king@gmail.com", "ton_mot_de_passe_app")
         server.send_message(msg)
 
-# Check des conditions
 btc_dom = get_btc_dominance()
 eth_btc = get_eth_btc_ratio()
 total_marketcap = get_total3_marketcap()
@@ -80,26 +85,85 @@ trends = get_trending_coins()
 f_g = get_fear_greed()
 sector_rotate = get_top_sectors()
 
-if (
-    btc_dom < 48                                                            # 1. BTC dominance en baisse
-    and eth_btc > 0.06                                                      # 2. ETH surperforme BTC
-    and total_marketcap["total3_usd"] > 150_000_000_000                     # 3. Market cap altcoins (TOTAL3) > 150B
-    and total3_volume["total_volume_usd"] > 30_000_000_000                  # 4. Volumes importants > 30B
-    and len(trends) >= 5                                                    # 5. Narratifs actifs (≥5 coins en tendance)
-    and int(f_g["value"]) > 60                                              # 6. Fear & Greed > 60 = Greed/FOMO
-    and any(cat["market_cap"] > 10_000_000_000 for cat in sector_rotate)    # 7. Rotation sectorielle : gros secteurs > 10B
-):
-    send_alert(
-        f"""🚨 Alerte potentielle Alt Season 🚨
-        BTC Dominance: {btc_dom:.2f}%
-        ETH/BTC: {eth_btc:.4f}
-        TOTAL3: {total_marketcap['total3_usd'] / 1e9:.2f}B$
-        Volume: {total3_volume['total_volume_usd'] / 1e9:.2f}B$
-        Fear & Greed: {f_g['value']} ({f_g['classification']})
-        Top sectors: {[cat['name'] for cat in sector_rotate[:3]]}
-        Coins tendances: {', '.join(trends[:5])}
-        ✅ Possible début d'Alt Season !
-        """
-    )
-else:
-    print("Rien à signaler 🚀")
+kpi_count = 0
+
+st.title('KPIs state')
+st.divider()
+
+left_col, right_col = st.columns([1, 1])
+with left_col:
+    if (btc_dom < 48):
+        st.header('Bitcoin dominance : ✅')
+        st.subheader(f"Valeur: {btc_dom:.2f}%")
+        kpi_count += 1
+    else:
+        st.header('Bitcoin dominance : ❌')
+        st.subheader(f"Valeur: {btc_dom:.2f}%")
+    st.caption("Quand la dominance du BTC baisse après une phase haussière prolongée → signal que les capitaux se déplacent vers les alts. Seuils souvent observés : une cassure baissière de la dominance sous 50-48 % est un déclencheur fréquent.")
+    st.divider()
+
+    if (eth_btc > 0.06):
+        st.header('ETH/BTC ratio : ✅')
+        st.subheader(f"Valeur: {eth_btc:.4f}")
+        kpi_count += 1
+    else:
+        st.header('ETH/BTC ratio : ❌')
+        st.subheader(f"Valeur: {eth_btc:.4f}")
+    st.caption("Quand l'ETH surperforme le BTC (ETH/BTC > 0,06) → signal que les investisseurs se tournent vers les altcoins. Seuils souvent observés : un ratio ETH/BTC supérieur à 0,06 indique une tendance haussière pour les alts.")
+    st.divider()
+
+    if (int(f_g["value"]) > 60):
+        st.header('Sentiment (Fear & Greed) : ✅')
+        st.subheader(f"Valeur: {f_g['value']} ({f_g['classification']})")
+        kpi_count += 1
+    else:
+        st.header('Sentiment (Fear & Greed) : ❌')
+        st.subheader(f"Valeur: {f_g['value']} ({f_g['classification']})")
+    st.caption("Quand l'indice de peur et de cupidité (Fear & Greed Index) est supérieur à 60 → signal que les investisseurs sont optimistes et se tournent vers les altcoins. Seuils souvent observés : un indice supérieur à 60 indique une tendance haussière pour les alts.")
+    st.divider()
+
+    if (total_marketcap["total3_usd"] > 150_000_000_000):
+        st.header('TOTAL3 marketcap : ✅')
+        st.subheader(f"Valeur: {total_marketcap['total3_usd'] / 1e9:.2f}B$")
+        kpi_count += 1
+    else:
+        st.header('TOTAL3 marketcap : ❌')
+        st.subheader(f"Valeur: {total_marketcap['total3_usd'] / 1e9:.2f}B$")
+    st.caption("Quand la capitalisation totale des altcoins (hors BTC & ETH) dépasse 150 milliards de dollars → signal que les investisseurs se tournent vers les altcoins. Seuils souvent observés : une capitalisation totale des altcoins supérieure à 150 milliards de dollars indique une tendance haussière pour les alts.")
+    st.divider()
+
+    if (total3_volume["total_volume_usd"] > 30_000_000_000):
+        st.header('TOTAL3 volume : ✅')
+        st.subheader(f"Valeur: {total3_volume['total_volume_usd'] / 1e9:.2f}B$")
+        kpi_count += 1
+    else:
+        st.header('TOTAL3 volume : ❌')
+        st.subheader(f"Valeur: {total3_volume['total_volume_usd'] / 1e9:.2f}B$")
+    st.caption("Quand le volume total des altcoins (hors BTC & ETH) dépasse 30 milliards de dollars → signal que les investisseurs se tournent vers les altcoins. Seuils souvent observés : un volume total des altcoins supérieur à 30 milliards de dollars indique une tendance haussière pour les alts.")
+    st.divider()
+
+    if (len(trends) >= 5):
+        st.header('Nombre de coins en tendance : ✅')
+        st.subheader(f"Valeur: {', '.join(trends[:5])}")
+        kpi_count += 1
+    else:
+        st.header('Nombre de coins en tendance : ❌')
+        st.subheader(f"Valeur: {', '.join(trends[:5])}")
+    st.caption("Quand au moins 5 coins sont en tendance sur CoinGecko → signal que les investisseurs se tournent vers les altcoins. Seuils souvent observés : la présence d'au moins 5 coins en tendance indique une tendance haussière pour les alts.")
+    st.divider()
+
+    if (any(cat["market_cap"] > 10_000_000_000 for cat in sector_rotate)):
+        st.header('Rotation sectorielle" : ✅')
+        st.subheader(f"Top sectors: {[cat['name'] for cat in sector_rotate[:3]]}")
+        kpi_count += 1
+    else:
+        st.header('Rotation sectorielle" : ❌')
+        st.subheader(f"Top sectors: {[cat['name'] for cat in sector_rotate[:3]]}")
+    st.caption("Quand au moins un secteur a une capitalisation de marché supérieure à 10 milliards de dollars → signal que les investisseurs se tournent vers les altcoins. Seuils souvent observés : la présence d'au moins un secteur avec une capitalisation de marché supérieure à 10 milliards de dollars indique une tendance haussière pour les alts.")
+    st.divider()
+with right_col:
+    st.header(f"Etats des indicateurs : {kpi_count} / 7")
+    notes = st.text_area("Ajoutez vos observations ici", height=800)
+    if st.button("Sauvegarder les notes"):
+
+        st.success("Notes sauvegardées!")
